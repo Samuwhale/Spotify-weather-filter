44ff8256b59501a67040a4ae3d5c5a0f982cba01
Samuwhale <sfspithorst@gmail.com>
2022-11-29 20:28:44 +0100

    Weather data
    - Added a notebook for quickly exploring data
    - WeatherManager now has data for weather and the average of the past five days at the same time-point.

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ from datetime import datetime
 from weather_manager import WeatherManager
 from spotify_manager import SpotifyManager
 
+# User lon lat data
+LON = 5.1214
+LAT = 52.0907
+
 sm = SpotifyManager()
-wm = WeatherManager()
+wm = WeatherManager(lon=LON, lat=LAT)
+
+
+print(wm.today, wm.historic_average)
+
 
 # sm.update_playlist()
 
--- weather_manager.py
+++ weather_manager.py
@@ -1,26 +1,89 @@
 import requests
 import os
+import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
 
-LON = 5.1214
-LAT = 52.0907
 APPID = os.getenv('OWD_KEY')
 ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
 
-PARAMS = {
-    "lon": LON,
-    "lat": LAT,
-    "appid": APPID,
-    "exclude": "minutely,hourly,daily,alerts"
-}
+columns = ["dt",
+           "sunrise",
+           "sunset",
+           "temp",
+           "feels_like",
+           "pressure",
+           "humidity",
+           "dew_point",
+           "uvi",
+           "clouds",
+           "visibility",
+           "wind_speed",
+           "wind_deg",
+           "wind_gust",
+           ]
 
 
 class WeatherManager:
-    def __init__(self):
-        self.temperature = 0
-        self.precip = 0
-        print(self.get_data())
+    def __init__(self, lon, lat):
+        self.lon = lon
+        self.lat = lat
+        self.energy = (0, 1)
+        self.dance = (0, 1)
+        self.valence = (0, 1)
+        self.today = self.get_current_weather()
+        self.historic_average = self.get_historic_weather()
 
-    def get_data(self):
-        self.response = requests.get(ENDPOINT, params=PARAMS)
-        self.response.raise_for_status()
-        return self.response.json()
+    def get_current_weather(self):
+        today_dict = {}
+        for column in columns:
+            today_dict[column] = []
+
+        curr_params = {
+            "lon": self.lon,
+            "lat": self.lat,
+            "appid": APPID,
+            "exclude": "minutely,hourly,daily,alerts",
+            "units": "metric"
+        }
+        curr_data = requests.get(ENDPOINT, params=curr_params)
+        curr_data.raise_for_status()
+        curr_data = curr_data.json()
+
+        for column in columns:
+            value = curr_data['current'][column]
+            today_dict[column] = value
+
+        return today_dict
+
+    def get_historic_weather(self):
+        past_five_dict = {}
+        for column in columns:
+            past_five_dict[column] = []
+
+        for i in range(1, 6):
+            five_days = datetime.utcnow() - timedelta(i)
+            hist_params = {
+                "lon": self.lon,
+                "lat": self.lat,
+                "appid": APPID,
+                "units": "metric",
+                "dt": str(five_days.timestamp()).split('.')[0],
+                "only_current": True
+            }
+            hist_data = requests.get(f"{ENDPOINT}/timemachine", params=hist_params)
+
+            hist_data.raise_for_status()
+            hist_data = hist_data.json()
+
+            for column in columns:
+                value = hist_data['current'][column]
+                past_five_dict[column].append(value)
+
+        average_dict = {}
+
+        for column in columns:
+            average_dict[column] = np.mean(past_five_dict[column])
+
+        return average_dict
+
